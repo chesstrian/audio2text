d8f21554b71099a3f7db5b982dec980b592c2dd3
Christian Gutierrez <chesstrian@gmail.com>
2016-11-25 16:53:52 -0500

    Convert aac files

--- convert_audios.sh
+++ convert_audios.sh
@@ -5,3 +5,9 @@ do
     f=$(echo $F | tr '[:upper:]' '[:lower:]')
     avconv -i "$F" "${f/%m4a/wav}"
 done
+
+for F in audios/*.aac
+do
+    f=$(echo $F | tr '[:upper:]' '[:lower:]')
+    avconv -i "$F" "${f/%aac/wav}"
+done
