52e890fe321a0304a02f4581ae0caaa88a392221
Christian Gutierrez <chesstrian@gmail.com>
2016-11-28 15:16:28 -0500

    Fix end and error events handling

--- src/index.js
+++ src/index.js
@@ -17,21 +17,21 @@ const params = {
 const speech2Text = watson.speech_to_text(setup);
 const recognizeStream = speech2Text.createRecognizeStream(params);
 
+recognizeStream.setEncoding('utf8');
+
+recognizeStream.on('data', function(event) { onEvent('Data:', event); });
+recognizeStream.on('results', function(event) { onEvent('Results:', event); });
+recognizeStream.on('end', function(event) { onEvent('End:', event); });
+recognizeStream.on('error', function(event) { onEvent('Error:', event); });
+recognizeStream.on('close', function(event) { onEvent('Close:', event); });
+
 fs.readdirSync(path.join(__dirname, '..', 'audios')).forEach(function (file) {
   if (file.endsWith('.wav')) {
     fs.createReadStream(path.join(__dirname, '../audios', file)).pipe(recognizeStream);
     recognizeStream.pipe(fs.createWriteStream(path.join(__dirname, '../texts', file.replace('.wav', '.txt'))));
-
-    recognizeStream.setEncoding('utf8');
-
-    recognizeStream.on('data', function(event) { onEvent('Data:', event); });
-    recognizeStream.on('results', function(event) { onEvent('Results:', event); });
-    recognizeStream.on('end', function(event) { onEvent('End:', event); });
-    recognizeStream.on('error', function(event) { onEvent('Error:', event); });
-    recognizeStream.on('close', function(event) { onEvent('Close:', event); process.exit(); });
   }
 });
 
 function onEvent(name, event) {
-  console.log(name, name != 'End:' ? JSON.stringify(event, null, 2) : 'End of transcription');
+  console.log(name, ~['End:', 'Error:'].indexOf(name) ? event : JSON.stringify(event, null, 2));
 }
